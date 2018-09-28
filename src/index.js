5e6f4ce2e709e5d650ef038da7e4b4367978a100
Victoria Udodava <43163503+Hentmire@users.noreply.github.com>
2018-09-28 10:59:32 +0300

    First solution (19/20)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 == 1) {
+  	return false;
+  }
+  for (var j = 0; j < bracketsConfig.length; j++) {
+	var a = bracketsConfig[j][0];
+	var b = bracketsConfig[j][1];
+	for (var m = 1; m <= str.length; m += 2) {
+		for (var i = 0; i < str.length; i++) {
+			for (var l = i + 1; l <= str.length; l++) {
+				if (a == str[i] && b == str[l]) {
+					if (l - i == m) {
+						str = str.slice(0, i) + str.slice(i+1, l) + str.slice(l+1);
+						i = -1; //после вырезки открыв. и закрыв. символа запускает цикл заново;
+					}
+				}
+			}
+		}
+	}
+}
+
+  if (str.length == 0) {
+  	return true;
+  }
+  return false;
 }
