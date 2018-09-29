4e19fd139469a1a9674be764f10acc2efbb53d8e
Victoria Udodava <43163503+Hentmire@users.noreply.github.com>
2018-09-29 16:12:05 +0300

    Correct solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str.length % 2 == 1) {
-  	return false;
-  }
-  for (var j = 0; j < bracketsConfig.length; j++) {
-	var a = bracketsConfig[j][0];
-	var b = bracketsConfig[j][1];
-	for (var m = 1; m <= str.length; m += 2) {
-		for (var i = 0; i < str.length; i++) {
-			for (var l = i + 1; l <= str.length; l++) {
-				if (a == str[i] && b == str[l]) {
-					if (l - i == m) {
-						str = str.slice(0, i) + str.slice(i+1, l) + str.slice(l+1);
-						i = -1; //после вырезки открыв. и закрыв. символа запускает цикл заново;
+	if (str.length % 2 == 1) {
+		return false;
+	}
+        var counter = 0;
+		for (var j = 0; j < bracketsConfig.length; j++) {
+			var a = bracketsConfig[j][0];
+			var b = bracketsConfig[j][1];
+			var arr = [];
+			for (var m = 0; m < str.length; m++) {
+				arr[m] = 0;
+			}
+			for (var i = 0; i < str.length; i++) {
+				for (var l = i + 1; l <= str.length; l++) {
+					if (a == str[i] && b == str[l] && arr[i] != 1 && arr[l] != 1) {
+						if ( (l - i) % 2 == 1) {
+							arr[i] = 1;
+							arr[l] = 1;
+							counter += 2;
+						}
 					}
 				}
 			}
 		}
-	}
-}
 
-  if (str.length == 0) {
-  	return true;
-  }
-  return false;
+	if (counter == str.length) {
+		return true;
+	}
+	return false;
 }
